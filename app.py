e3c12870b45503b0acf4bb7f9ff30b1709238444
Perry <pmatovu12@hotmail.co.uk>
2023-06-16 14:24:36 +0100

    update team details

--- app.py
+++ app.py
@@ -7,12 +7,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-
 @app.route('/players', methods=['GET'])
 def get_players():
     player_dict = players.get_players()
     return jsonify(player_dict)
-
 
 @app.route('/team/<teamId>', methods=['GET'])
 def team_profile(teamId):
@@ -22,19 +20,22 @@ def team_profile(teamId):
     else:
         return jsonify({'message': 'Team not found'}), 404
 
-
 @app.route('/teams', methods=['GET'])
 def teams():
     teams = nba_teams.get_teams()
     return jsonify(teams)
 
-
 @app.route('/teamdetails/<teamId>', methods=['GET'])
 def get_team_details(teamId):
     details = teamdetails.TeamDetails(team_id=teamId)
 
-    return jsonify(details)
+    # Parsing the details as JSON might not be straightforward because of the data structure.
+    # We'll convert each dataset to a list of dictionaries and store them in a new dictionary.
+    details_dict = {}
+    for dataset in details.data_sets:
+        details_dict[dataset] = [item.__dict__ for item in getattr(details, dataset)]
 
+    return jsonify(details_dict)
 
 if __name__ == '__main__':
     app.run(debug=True)
