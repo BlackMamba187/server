66c53755430e8b6716b016c6a642174134acfcb3
Perry <pmatovu12@hotmail.co.uk>
2023-06-16 15:01:40 +0100

    team details and current roster

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import Flask, jsonify, make_response, request
 from nba_api.stats.static import players, teams
 import nba_api.stats.static.teams as nba_teams
-from nba_api.stats.endpoints import teamdetails
+from nba_api.stats.endpoints import teamdetails, commonteamroster
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route('/players', methods=['GET'])
 def get_players():
     player_dict = players.get_players()
     return jsonify(player_dict)
+
 
 @app.route('/team/<teamId>', methods=['GET'])
 def team_profile(teamId):
@@ -20,22 +22,28 @@ def team_profile(teamId):
     else:
         return jsonify({'message': 'Team not found'}), 404
 
+
 @app.route('/teams', methods=['GET'])
 def teams():
     teams = nba_teams.get_teams()
     return jsonify(teams)
 
+
 @app.route('/teamdetails/<teamId>', methods=['GET'])
 def get_team_details(teamId):
     details = teamdetails.TeamDetails(team_id=teamId)
+    data = details.get_normalized_dict()
 
-    # Parsing the details as JSON might not be straightforward because of the data structure.
-    # We'll convert each dataset to a list of dictionaries and store them in a new dictionary.
-    details_dict = {}
-    for dataset in details.data_sets:
-        details_dict[dataset] = [item.__dict__ for item in getattr(details, dataset)]
+    return jsonify(data)
 
-    return jsonify(details_dict)
+
+@app.route('/commonteamroster/<team_id>/<season>', methods=['GET'])
+def team_roster(team_id, season):
+    roster = commonteamroster.CommonTeamRoster(team_id=team_id, season=season)
+    data_sets = roster.get_normalized_dict()
+
+    return jsonify(data_sets)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
