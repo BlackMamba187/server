6fe0992ac80d5ab6773b7cd474177a4eb014e25d
Perry <pmatovu12@hotmail.co.uk>
2023-06-16 12:59:07 +0100

    teamId

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, make_response, request
 from nba_api.stats.static import players, teams
+import nba_api.stats.static.teams as nba_teams
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -10,10 +11,17 @@ def get_players():
     player_dict = players.get_players()
     return jsonify(player_dict)
 
-@app.route('/teams', methods=['GET'])
-def get_teams():
-    team_dict = teams.get_teams()
-    return jsonify(team_dict)
+@app.route('/team/<teamId>', methods=['GET'])
+def team_profile(teamId):
+    team = nba_teams.find_team_name_by_id(teamId)
+    if team:
+        return jsonify(team)
+    else:
+        return jsonify({'message': 'Team not found'}), 404
 
+@app.route('/teams', methods=['GET'])
+def teams():
+    teams = nba_teams.get_teams()
+    return jsonify(teams)
 if __name__ == '__main__':
     app.run(debug=True)
