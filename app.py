d5f39d5fc2e7b28147e21cf73bc1140b592ef670
Perry <pmatovu12@hotmail.co.uk>
2023-06-18 15:09:52 +0100

    update

--- app.py
+++ app.py
@@ -1,30 +1,33 @@
-from flask import Flask, jsonify, make_response, request
-from nba_api.stats.static import players, teams
-import nba_api.stats.static.teams as nba_teams
+from flask import Flask, jsonify
 from flask_cors import CORS
+from nba_api.stats.static import players, teams
+from nba_api.stats.endpoints import commonplayerinfo
+from nba_api import debug  # Import debug from nba_api
+
+import json
+
+# Set the PROXY value
+PROXY = '127.0.0.1:80'  # Replace with your proxy address and port
 
 app = Flask(__name__)
 CORS(app)
-
 
 @app.route('/players', methods=['GET'])
 def get_players():
     player_dict = players.get_players()
     return jsonify(player_dict)
 
-
 @app.route('/team/<teamId>', methods=['GET'])
 def team_profile(teamId):
-    team = nba_teams.find_team_name_by_id(teamId)
+    team = teams.find_team_name_by_id(teamId)
     if team:
         return jsonify(team)
     else:
         return jsonify({'message': 'Team not found'}), 404
 
-
 @app.route('/teams', methods=['GET'])
-def teams():
-    teams = nba_teams.get_teams()
+def get_teams():
+    teams = teams.get_teams()
     return jsonify(teams)
 
 if __name__ == '__main__':
