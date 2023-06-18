5c83bdf2f7740ce37148cb77bc20d35241adacd3
Perry <pmatovu12@hotmail.co.uk>
2023-06-18 15:17:45 +0100

    proxy

--- app.py
+++ app.py
@@ -3,13 +3,18 @@ from flask_cors import CORS
 from nba_api.stats.static import players, teams
 from nba_api.stats.endpoints import commonplayerinfo
 
+# Set the PROXY value
+PROXY = '127.0.0.1:80'
+
 app = Flask(__name__)
 CORS(app)
+
 
 @app.route('/players', methods=['GET'])
 def get_players():
     player_dict = players.get_players()
     return jsonify(player_dict)
+
 
 @app.route('/team/<teamId>', methods=['GET'])
 def team_profile(teamId):
@@ -19,10 +24,20 @@ def team_profile(teamId):
     else:
         return jsonify({'message': 'Team not found'}), 404
 
+
 @app.route('/teams', methods=['GET'])
 def get_teams():
     teams = teams.get_teams()
     return jsonify(teams)
+
+
+@app.route('/commonplayerinfo/<playerId>', methods=['GET'])
+def get_common_player_info(playerId):
+    info = commonplayerinfo.CommonPlayerInfo(
+        player_id=playerId, proxy=PROXY, timeout=100)
+    data = info.get_normalized_dict()
+    return jsonify(data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
