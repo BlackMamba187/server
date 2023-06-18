149ff27ad20db3164bd5665369e4b62d94da2e6f
Perry <pmatovu12@hotmail.co.uk>
2023-06-18 15:36:37 +0100

    update

--- app.py
+++ app.py
@@ -6,16 +6,6 @@ from nba_api.stats.endpoints import commonplayerinfo
 app = Flask(__name__)
 CORS(app)
 
-custom_headers = {
-    'Host': 'stats.nba.com',
-    'Connection': 'keep-alive',
-    'Cache-Control': 'max-age=0',
-    'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'en-US,en;q=0.9',
-}
 
 @app.route('/players', methods=['GET'])
 def get_players():
@@ -41,7 +31,7 @@ def get_teams():
 @app.route('/commonplayerinfo/<playerId>', methods=['GET'])
 def get_common_player_info(playerId):
     info = commonplayerinfo.CommonPlayerInfo(
-        player_id=playerId, proxy='127.0.0.1:80', timeout=100, headers=custom_headers)
+        player_id=playerId, timeout=100)
     data = info.get_normalized_dict()
     return jsonify(data)
 
