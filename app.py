9b23ee8a57b82e35f9dbccdd121be584c517ece2
Perry <pmatovu12@hotmail.co.uk>
2023-06-22 18:37:58 +0100

    changed to scraper

--- app.py
+++ app.py
@@ -1,46 +1,57 @@
-import requests
 from flask import Flask, jsonify
 from flask_cors import CORS
-from nba_api.stats.static import players, teams as nba_teams
-from nba_api.stats.endpoints import commonplayerinfo
+import requests
+from bs4 import BeautifulSoup
+import json
 
 app = Flask(__name__)
 CORS(app)
 
-NBA_API_HEADERS = {
-    'Referer': 'https://nba-server-s0rg.onrender.com',
-    'Origin': 'https://nba-server-s0rg.onrender.com',
-    'x-nba-stats-origin': 'stats',
-    'x-nba-stats-token': 'true',
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'
-}
-
-@app.route('/players', methods=['GET'])
+@app.route('/activeteams', methods=['GET'])
 def get_players():
-    player_dict = players.get_players()
-    return jsonify(player_dict)
+    def parse_row(row):
+        team_name = row.th.text
+        team_data = {
+            'Lg': row.find('td', {'data-stat': 'lg_id'}).text,
+            'From': row.find('td', {'data-stat': 'year_min'}).text,
+            'To': row.find('td', {'data-stat': 'year_max'}).text,
+            'Yrs': row.find('td', {'data-stat': 'years'}).text,
+            'Games': row.find('td', {'data-stat': 'g'}).text,
+            'Wins': row.find('td', {'data-stat': 'wins'}).text,
+            'Losses': row.find('td', {'data-stat': 'losses'}).text,
+            'Win_Loss_Percentage': row.find('td', {'data-stat': 'win_loss_pct'}).text,
+            'Plyfs': row.find('td', {'data-stat': 'years_playoffs'}).text,
+            'Div': row.find('td', {'data-stat': 'years_division_champion'}).text,
+            'Conf': row.find('td', {'data-stat': 'years_conference_champion'}).text,
+            'Champ': row.find('td', {'data-stat': 'years_league_champion'}).text,
+        }
+        return team_name, team_data
 
+    def parse_table(html_table):
+        soup = BeautifulSoup(html_table, 'html.parser')
+        rows = soup.find_all('tr')
 
-@app.route('/teams', methods=['GET'])
-def get_teams():
-    teams = nba_teams.get_teams()
-    return jsonify(teams)
+        franchise_data = {}
+        for row in rows:
+            if row.has_attr('class') and 'full_table' in row['class']:
+                franchise_name, franchise_info = parse_row(row)
+                franchise_data[franchise_name] = {'Current': franchise_info, 'History': {}}
+            elif row.has_attr('class') and 'partial_table' in row['class']:
+                team_name, team_info = parse_row(row)
+                if franchise_name in franchise_data:
+                    franchise_data[franchise_name]['History'][team_name] = team_info
 
+        return franchise_data
 
-@app.route('/team/<teamId>', methods=['GET'])
-def team_profile(teamId):
-    team = nba_teams.find_team_name_by_id(teamId)
-    if team:
-        return jsonify(team)
-    else:
-        return jsonify({'message': 'Team not found'}), 404
+    url = 'https://www.basketball-reference.com/teams/'
+    response = requests.get(url)
+    html_content = response.text
 
+    soup = BeautifulSoup(html_content, 'html.parser')
+    html_table = str(soup.find('table', {'id': 'teams_active'}))
 
-@app.route('/commonplayerinfo/<playerId>', methods=['GET'])
-def get_common_player_info(playerId):
-    info = commonplayerinfo.CommonPlayerInfo(player_id=playerId, headers=NBA_API_HEADERS)
-    data = info.get_normalized_dict()
-    return jsonify(data)
+    franchise_data = parse_table(html_table)
+    return jsonify(franchise_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ object.ts
@@ -0,0 +1,19 @@
+const active_teams = {
+	"Chicago Bulls": {
+        "Current": {
+            "Lg": "NBA",
+            "From": "1966-67",
+            "To": "2022-23",
+            "Yrs": "57",
+            "Games": "4598",
+            "Wins": "2344",
+            "Losses": "2254",
+            "Win_Loss_Percentage": ".510",
+            "Plyfs": "37",
+            "Div": "9",
+            "Conf": "6",
+            "Champ": "6"
+        },
+        "History": {}
+    },
+};
